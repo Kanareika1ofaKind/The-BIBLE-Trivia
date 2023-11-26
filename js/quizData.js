56c1eca3e40f0e79662b5abf4960885309a7083e
Sydney <proteimus@gmail.com>
2023-11-26 09:44:14 -0600

    1stLaunch

---
+++ js/quizData.js
@@ -0,0 +1,154 @@
+const quizData = [
+    {
+        title: "Old Testement",
+        image: "img/eden.png",
+        questions: [
+            {
+                question: "The Bible is the most popular book ever written",
+                answers: {
+                    a: 'true',
+                    b: 'false'
+                },
+                correctAnswer: 'a'
+            },
+            {
+                question: "question goes here (2)?",
+                answers: {
+                    a: 'true',
+                    b: 'false'
+                },
+                correctAnswer: 'a'
+            },
+            {
+                question: "question goes here (3)?",
+                answers: {
+                    a: 'true',
+                    b: 'false'
+                },
+                correctAnswer: 'a'
+            }
+        ]
+    },
+    {
+        title: "question set 2 title",
+        image: "img/eden1.png",
+        questions: [
+            {
+                question: "question goes here (1)?",
+                answers: {
+                    a: 'true',
+                    b: 'false'
+                },
+                correctAnswer: 'a'
+            },
+            {
+                question: "question goes here (2)?",
+                answers: {
+                    a: 'true',
+                    b: 'false'
+                },
+                correctAnswer: 'a'
+            },
+            {
+                question: "question goes here (3)?",
+                answers: {
+                    a: 'true',
+                    b: 'false'
+                },
+                correctAnswer: 'a'
+            },
+            {
+                question: "question goes here (1)?",
+                answers: {
+                    a: 'true',
+                    b: 'false'
+                },
+                correctAnswer: 'a'
+            },
+            {
+                question: "question goes here (2)?",
+                answers: {
+                    a: 'true',
+                    b: 'false'
+                },
+                correctAnswer: 'a'
+            },
+            {
+                question: "question goes here (3)?",
+                answers: {
+                    a: 'true',
+                    b: 'false'
+                },
+                correctAnswer: 'a'
+            },
+            {
+                question: "question goes here (1)?",
+                answers: {
+                    a: 'true',
+                    b: 'false'
+                },
+                correctAnswer: 'a'
+            },
+            {
+                question: "question goes here (2)?",
+                answers: {
+                    a: 'true',
+                    b: 'false'
+                },
+                correctAnswer: 'a'
+            },
+            {
+                question: "question goes here (3)?",
+                answers: {
+                    a: 'true',
+                    b: 'false'
+                },
+                correctAnswer: 'a'
+            }
+        ]
+    },
+    {
+        title: "question set 3 title",
+        image: "img/eden3.png",
+        questions: [
+            {
+                question: "question goes here (1)?",
+                answers: {
+                    a: 'true',
+                    b: 'false'
+                },
+                correctAnswer: 'a'
+            },
+            {
+                question: "question goes here (2)?",
+                answers: {
+                    a: 'true',
+                    b: 'false'
+                },
+                correctAnswer: 'a'
+            }
+        ]
+    },
+    {
+        title: "question set 4 title",
+        image: "img/eden3.png",
+        questions: [
+            {
+                question: "question goes here (1)?",
+                answers: {
+                    a: 'true',
+                    b: 'false'
+                },
+                correctAnswer: 'a'
+            },
+            {
+                question: "question goes here (2)?",
+                answers: {
+                    a: 'true',
+                    b: 'false'
+                },
+                correctAnswer: 'a'
+            }
+        ]
+    }
+]
