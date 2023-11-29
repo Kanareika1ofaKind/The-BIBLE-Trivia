aac824d90efdef83dc25285b63763a80e6e2bd4f
Sydney <proteimus@gmail.com>
2023-11-29 10:24:56 -0600

    data entry

--- js/quizData.js
+++ js/quizData.js
@@ -1,154 +1,401 @@
 const quizData = [
     {
-        title: "Old Testement",
-        image: "img/eden.png",
+        title: "Bible Trivia - Old Testement",
+        image: "img/eden17.png",
         questions: [
             {
                 question: "The Bible is the most popular book ever written",
                 answers: {
                     a: 'true',
-                    b: 'false'
+                    b: 'False'
                 },
                 correctAnswer: 'a'
             },
             {
-                question: "question goes here (2)?",
+                question: "What is the first book in the Bible?",
                 answers: {
-                    a: 'true',
-                    b: 'false'
+                    a: 'Revelation',
+                    b: 'Genesis',
+                    c: 'Ecclesiastes',
+                    d: '1 Chronicles'
+                },
+                correctAnswer: 'b'
+            },
+            {
+                question: "How many days did God take to create the world?",
+                answers: {
+                    a: '40',
+                    b: '7',
+                    c: '6',
+                    d: '1'
+                },
+                correctAnswer: 'c'
+            },
+            {
+                question: "Who was the first man?",
+                answers: {
+                    a: 'Jesus',
+                    b: 'Moses',
+                    c: 'John',
+                    d: 'Adam'
+                },
+                correctAnswer: 'd'
+            },
+            {
+                question: "Who was the first woman?",
+                answers: {
+                    a: 'Ruth',
+                    b: 'Lizabeth',
+                    c: 'Eve',
+                    d: 'Maria'
+                },
+                correctAnswer: 'c'
+
+            }
+        ]
+    },
+    // 2nd Page
+    {
+        title: "Bible Trivia - Signs && Miracles from Above",
+        image: "img/light2.png",
+        questions: [
+            {
+                question: "What does the Bible say about the purpose of the rainbow?",
+                answers: {
+                    a: 'To be a sign of good luck',
+                    b: 'To be an everlasting reminder of God’s covenant with man',
+                    c: 'To be a sign of God’s wrath',
+                    d: 'To be a sign of God’s judgment'
+                },
+                correctAnswer: 'b'
+            },
+            {
+                question: "What were the talking animals in the Bible?",
+                answers: {
+                    a: 'whale & goat',
+                    b: 'fish & goat',
+                    c: 'snake & donkey',
+                    d: 'snake & horse'
+                },
+                correctAnswer: 'c'
+            },
+            {
+                question: "Who brought bread and meat to Elijah?",
+                answers: {
+                    a: 'raven',
+                    b: 'wolf',
+                    c: 'bear',
+                    d: 'lion'
                 },
                 correctAnswer: 'a'
             },
             {
-                question: "question goes here (3)?",
+                question: "John Joseph Nouri claimed to have discovered Noah's Ark on the summit of Mount _____ in April 1887?",
                 answers: {
-                    a: 'true',
-                    b: 'false'
+                    a: 'Mount Gerizim',
+                    b: 'Mount Sinai',
+                    c: 'Mount Carmel',
+                    d: 'Mount Ararat'
+                },
+                correctAnswer: 'd'
+            },
+            {
+                question: "How old was Sarah, when she gave birth to her firstborn son, Isaak?",
+                answers: {
+                    a: '100',
+                    b: '98',
+                    c: '90',
+                    d: '110'
+                },
+                correctAnswer: 'c'
+            }
+               
+        ]
+    },
+     // 3rd Page
+    {
+        title: "Bible Trivia - Famous man in Old Testament",
+        image: "img/eden11.png",
+        questions: [
+            {
+                question: "This man wrote the first five books of the Old Testament. ...?",
+                answers: {
+                    a: 'Adam',
+                    b: 'Moses',
+                    c: 'Abraham',
+                    d: 'Aaron'
+                },
+                correctAnswer: 'b'
+            },
+            {
+                question: "A godly man in an ungodly age. This man married Ruth of Moab and became great grandfather of King David. Who is this godly man?",
+                answers: {
+                    a: 'Balaam',
+                    b: 'Bildad',
+                    c: 'Belshazzar',
+                    d: 'Boaz'
+                },
+                correctAnswer: 'd'
+            },
+            {
+                question: "A righteous man whose severe trials and enduring faith are the subject of the book that bears his name. The book is a series of dialogues between this man and his friends regarding the cause of his suffering.Who is this righteous man?",
+                answers: {
+                    a: 'Job',
+                    b: 'Joab',
+                    c: 'Joel',
+                    d: 'Jeshua'
+                },
+                correctAnswer: 'a'
+            },
+            {
+                question: " A prophet the Lord sent to warn Nineveh that the city was about to suffer destruction. This prophet tried to run from God but eventually delivered the message, prompting the city to respond in fear and repentance. Who is this prophet?",
+                answers: {
+                    a: 'Jonathan',
+                    b: 'Joab ',
+                    c: 'Jonah',
+                    d: 'Joseph'
+                },
+                correctAnswer: 'c'
+            },
+            {
+                question: "Who was the prince of Israel and David's most faithful friend?",
+                answers: {
+                    a: 'Jonathan',
+                    b: 'Joab ',
+                    c: 'Jonah',
+                    d: 'Joseph'
                 },
                 correctAnswer: 'a'
             }
         ]
     },
+     // 4th Page
     {
-        title: "question set 2 title",
-        image: "img/eden1.png",
+        title: "Bible Trivia - Famous woman in Old Testament",
+        image: "img/eden6.png",
         questions: [
             {
-                question: "question goes here (1)?",
+                question: "As the only woman who served as a “Judge” of Israel, she led the Israelites to a military victory over Sisera of Damascus. Who was this leader and widely known judge?",
                 answers: {
-                    a: 'true',
-                    b: 'false'
+                    a: 'Sarah',
+                    b: 'Leah',
+                    c: 'Deborah',
+                    d: 'Bathsheba'
+                },
+                correctAnswer: 'c'
+            },
+            {
+                question: "The mother-in-law of Ruth, who brought the young widow with her when she returned to Bethlehem from Moab after the death of her husband and both of her sons. She is th one, who advised Ruth to seek protection from Boaz, resulting in Ruth’s eventual marriage to the rich kinsman. Who is she?",
+                answers: {
+                    a: 'Miriam',
+                    b: 'Naomi',
+                    c: 'Rachel',
+                    d: 'Elizabeth'
+                },
+                correctAnswer: 'b'
+            },
+            {
+                question: "A prostitute of the pagan city of Jericho at the time the Israelites were about to invade. She hid the Israelite spies in exchange for their protection in the coming battle. After the spies kept their word and spared her at the fall of the city, she married an Israelite, becoming one of the ancestors of Jesus Christ. What was the name of this brave woman?",
+                answers: {
+                    a: 'Rahab',
+                    b: 'Rachel',
+                    c: 'Ramila',
+                    d: 'Rebekah'
                 },
                 correctAnswer: 'a'
             },
             {
-                question: "question goes here (2)?",
+                question: "A beautiful Jewess, whom the Persian king Ahasuerus married and made his queen.",
                 answers: {
-                    a: 'true',
-                    b: 'false'
+                    a: 'Kameliah',
+                    b: 'Keva',
+                    c: 'Ezdra',
+                    d: 'Esther'
+                },
+                correctAnswer: 'd'
+            },
+            {
+                question: "The first woman, created from the rib of the first man, Adam.",
+                answers: {
+                    a: 'Leah',
+                    b: 'Rebeccah',
+                    c: 'Sarah',
+                    d: 'Dinah'
+                },
+                correctAnswer: 'd'
+            }
+        ]
+    },
+     // 5th Page
+    {
+        title: "Bible Trivia - New Testement",
+        image: "img/light4.png",
+        questions: [
+
+            {
+                question: "In what town was Jesus born?",
+                answers: {
+                    a: 'Capernaum',
+                    b: 'Bethlehem',
+                    c: 'Nazareth',
+                    d: 'Jerusalem'
+                },
+                correctAnswer: 'b'
+            },
+            {
+                question: "In what city did Mary, Martha, and Lazarus live?",
+                answers: {
+                    a: 'Joppa',
+                    b: 'Bethany',
+                    c: 'Caesarea',
+                    d: 'Bethlehem'
+                },
+                correctAnswer: 'b'
+            },
+            {
+                question: "Who betrayed Jesus for 30 pieces of silver? (*In old times, this ammount was the price of the slave)",
+                answers: {
+                    a: 'Andrew',
+                    b: 'John',
+                    c: 'James',
+                    d: 'Judas Iscariot'
+                },
+                correctAnswer: 'd'
+            },
+                        {
+                question: "Who rolled the stone closing Jesus` tomb?",
+                answers: {
+                    a: 'Joshua',
+                    b: 'Thomas',
+                    c: 'Joseph',
+                    d: 'Matthew'
+                },
+                correctAnswer: 'c'
+            },
+            {
+                question: "Which event were in celebration, when Jesus turned water to wine for guests?",
+                answers: {
+                    a: 'a birthday',
+                    b: 'a wedding',
+                    c: 'a harvest',
+                    d: 'the Passover'
+                },
+                correctAnswer: 'b'
+            },
+        ]
+    },
+     // 6th Page
+        {
+        title: "Bible Trivia - Angels from the Bible",
+        image: "img/angelsO.jpeg",
+        questions: [
+            {
+                question: "What was the name of the angel of the Lord who appeared to Zacharias?",
+                answers: {
+                    a: 'Gabriel',
+                    b: ' Raphael',
+                    c: 'Donotello',
+                    d: 'Michei'
                 },
                 correctAnswer: 'a'
             },
             {
-                question: "question goes here (3)?",
+                question: "They have six wings and Isaiah refers to them after he has an experience with one of them. What kind of angels are these?",
                 answers: {
-                    a: 'true',
-                    b: 'false'
+                    a: 'dominions',
+                    b: 'seraphim',
+                    c: 'cherubim',
+                    d: 'principalities'
                 },
-                correctAnswer: 'a'
+                correctAnswer: 'b'
             },
             {
-                question: "question goes here (1)?",
+                question: "Which of these books in the Bible records the most angelic activity?",
                 answers: {
-                    a: 'true',
-                    b: 'false'
+                    a: 'Hewbrew',
+                    b: '1st Timothy',
+                    c: 'Revelation',
+                    d: 'Romans'
                 },
-                correctAnswer: 'a'
+                correctAnswer: 'c'
             },
             {
-                question: "question goes here (2)?",
+                question: "Out of the four Gospels, only one contains the name of Gabriel, the angel who appeared to the mother of Jesus and the father of John the Baptist. What's the name of the Gospel?",
                 answers: {
-                    a: 'true',
-                    b: 'false'
+                    a: 'Mark',
+                    b: 'Luke',
+                    c: 'Matthew',
+                    d: 'John'
                 },
-                correctAnswer: 'a'
+                correctAnswer: 'b'
             },
             {
-                question: "question goes here (3)?",
+                question: 'What three men were saved from the furnace by "an angel of the Lord"?',
                 answers: {
-                    a: 'true',
-                    b: 'false'
-                },
-                correctAnswer: 'a'
-            },
-            {
-                question: "question goes here (1)?",
-                answers: {
-                    a: 'true',
-                    b: 'false'
-                },
-                correctAnswer: 'a'
-            },
-            {
-                question: "question goes here (2)?",
-                answers: {
-                    a: 'true',
-                    b: 'false'
-                },
-                correctAnswer: 'a'
-            },
-            {
-                question: "question goes here (3)?",
-                answers: {
-                    a: 'true',
-                    b: 'false'
+                    a: 'Shadrach, Meshach and Abednego',
+                    b: 'Shem, Ham and Japheth',
+                    c: ' Reuben, Simeon and Levi',
+                    d: 'Asher, Issachar and Zebulun'
                 },
                 correctAnswer: 'a'
             }
         ]
     },
-    {
-        title: "question set 3 title",
-        image: "img/eden3.png",
+       // 7th Page
+        {
+        title: "Bible Trivia - Wisdom from the Holy Scripture",
+        image: "img/oldWiseMan.jpeg",
         questions: [
             {
-                question: "question goes here (1)?",
+                question: "WISDOM IS MORE PRECIOUS THAN...",
                 answers: {
-                    a: 'true',
-                    b: 'false'
+                    a: 'Pearls',
+                    b: 'Diamonds',
+                    c: 'Rubies',
+                    d: 'Emeralds'
                 },
-                correctAnswer: 'a'
+                correctAnswer: 'c'
             },
             {
-                question: "question goes here (2)?",
+                question: "To everything there is a ________, and a time to every purpose under the heaven.",
                 answers: {
-                    a: 'true',
-                    b: 'false'
+                    a: 'place under the sun',
+                    b: 'reason',
+                    c: 'day',
+                    d: 'season'
                 },
-                correctAnswer: 'a'
-            }
-        ]
-    },
-    {
-        title: "question set 4 title",
-        image: "img/eden3.png",
-        questions: [
-            {
-                question: "question goes here (1)?",
-                answers: {
-                    a: 'true',
-                    b: 'false'
-                },
-                correctAnswer: 'a'
+                correctAnswer: 'd'
             },
             {
-                question: "question goes here (2)?",
-                answers: {
-                    a: 'true',
-                    b: 'false'
-                },
-                correctAnswer: 'a'
-            }
+            question: "The salvation for Israel would come out of _____",
+            answers: {
+                   a: 'Israel',
+                   b: 'Zion',
+                   c: 'Jacob',
+                   d: 'Glory'
+                 },
+                correctAnswer: 'b'
+            },
+            {
+            question: "When does the Bible say we will find God?",
+            answers: {
+                   a: 'When we get baptized',
+                   b: 'When we seek Him',
+                   c: 'When we give to charity',
+                   d: 'When we work hard'
+                 },
+                correctAnswer: 'b'
+            },
+            {
+            question: "When will God show us great and mighty things which we do not yet know?",
+            answers: {
+                   a: 'When we attend church service',
+                   b: 'When we confess our sin',
+                   c: 'When the world comes to an end',
+                   d: 'When we call to Him'
+                 },
+                correctAnswer: 'd'
+            },
         ]
-    }
+    }  
 ]
